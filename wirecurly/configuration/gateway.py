e0cb8b8110f81e2fa0a8e9ac63929da1750485d4
Joao Mesquita <jmesquita@indicium.com.ar>
2014-01-20 16:50:32 -0300

    Greatly improve documentation.

--- wirecurly/configuration/gateway.py
+++ wirecurly/configuration/gateway.py
@@ -3,48 +3,54 @@ import logging
 log = logging.getLogger(__name__)
 
 __all__ = ['Gateway']
-		
+        
 class Gateway(object):
-	"""A gateway object"""
-	def __init__(self, name):
-		super(Gateway, self).__init__()
-		self.name = name
-		self.parameters = []
+    """A gateway object"""
+    def __init__(self, name):
+        super(Gateway, self).__init__()
+        self.name = name
+        self.parameters = []
 
 
-	def addParameter(self, param, val):
-		'''
-			Set an extra parameter for a gateway
-		'''
-		if param != 'rtp-ip':
-			try:
-				self.getParameter(param)
-			except ValueError:
-				self.parameters.append({'name': param, 'value': val})
-				return
-		else:
-			self.parameters.append({'name': param, 'value': val})
-                        return
-		log.warning('Cannot replace existing parameter.')
-		raise ValueError
+    def addParameter(self, param, val):
+        '''Set an extra parameter for a gateway
 
-	def getParameter(self, param):
-		'''
-			Retrieve the value of a parameter by its name
-		'''
-		for p in self.parameters:
-			if p.get('name') == param:
-				return p.get('value')
+        :param param: The paramenter to add
+        :type param: str
+        :param val: The value of the paramenter
+        :type val: str
+        :raises: ValueError -- in case the param already exists
+        '''
+        try:
+            self.getParameter(param)
+        except ValueError:
+            self.parameters.append({'name': param, 'value': val})
+            return
 
-		raise ValueError
+        log.warning('Cannot replace existing parameter.')
+        raise ValueError
+
+    def getParameter(self, param):
+        '''Retrieve the value of a parameter by its name
+
+        :rtype: str
+
+        :raises: ValueError -- in case the param does not exist
+        '''
+        for p in self.parameters:
+            if p.get('name') == param:
+                return p.get('value')
+
+        raise ValueError
 
 
-	def todict(self):
-		'''
-			Create a dict so it can be converted/serialized
-		'''
-		if self.parameters:
-			children =[{'tag': 'param', 'attrs': p} for p in self.parameters]
-		
-	
-		return {'tag': 'gateway', 'children': children, 'attrs': {'name': self.name}}
+    def todict(self):
+        '''Create a dict so it can be converted/serialized
+
+        :rtype: dict -- a dict ready to be serialized
+        '''
+        if self.parameters:
+            children =[{'tag': 'param', 'attrs': p} for p in self.parameters]
+        
+    
+        return {'tag': 'gateway', 'children': children, 'attrs': {'name': self.name}}
